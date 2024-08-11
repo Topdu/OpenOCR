73ca3a82477c5170b85d331a4c6fd9d2e7594169
Topdu <784990967@qq.com>
2024-08-11 22:07:11 +0800

    add iccv23 maerec and eccv20 autostr

--- openrec/modeling/encoders/vit.py
+++ openrec/modeling/encoders/vit.py
@@ -28,22 +28,37 @@ class ViT(nn.Module):
         act_layer=nn.GELU,
         last_stage=False,
         feat2d=False,
+        use_cls_token=False,
         **kwargs,
     ):
         super().__init__()
         self.img_size = img_size
         self.embed_dim = embed_dim
         self.out_channels = embed_dim
+        self.use_cls_token = use_cls_token
         self.feat_sz = [
             img_size[0] // patch_size[0], img_size[1] // patch_size[1]
         ]
+
         self.patch_embed = PatchEmbed(img_size, patch_size, in_channels,
                                       embed_dim)
         num_patches = self.patch_embed.num_patches
-        self.pos_embed = nn.Parameter(
-            torch.zeros([1, num_patches, embed_dim], dtype=torch.float32),
-            requires_grad=True,
-        )
+        if use_cls_token:
+            self.cls_token = nn.Parameter(
+                torch.zeros([1, 1, embed_dim], dtype=torch.float32),
+                requires_grad=True,
+            )
+            trunc_normal_(self.cls_token, mean=0, std=0.02)
+            self.pos_embed = nn.Parameter(
+                torch.zeros([1, num_patches + 1, embed_dim],
+                            dtype=torch.float32),
+                requires_grad=True,
+            )
+        else:
+            self.pos_embed = nn.Parameter(
+                torch.zeros([1, num_patches, embed_dim], dtype=torch.float32),
+                requires_grad=True,
+            )
         self.pos_drop = nn.Dropout(p=drop_rate)
 
         dpr = np.linspace(0, drop_path_rate, depth)
@@ -89,11 +104,15 @@ class ViT(nn.Module):
 
     def forward(self, x):
         x = self.patch_embed(x)
+        if self.use_cls_token:
+            x = torch.concat([self.cls_token.tile([x.shape[0], 1, 1]), x], 1)
         x = x + self.pos_embed
         x = self.pos_drop(x)
         for blk in self.blocks:
             x = blk(x)
         x = self.norm(x)
+        if self.use_cls_token:
+            x = x[:, 1:, :]
         if self.last_stage:
             x, sz = self.stages(x, self.feat_sz)
         if self.feat2d:
