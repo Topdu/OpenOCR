bd9650cdc0b40358b1765f013ccd5ecbd3b5f60c
dancer <22210240395@m.fudan.edu.cn>
2024-10-09 23:58:55 +0800

    add dptr pretrain

--- openrec/preprocess/__init__.py
+++ openrec/preprocess/__init__.py
@@ -23,6 +23,7 @@ from .smtr_label_encode import SMTRLabelEncode
 from .srn_label_encode import SRNLabelEncode
 from .visionlan_label_encode import VisionLANLabelEncode
 from .cam_label_encode import CAMLabelEncode
+from .dptr_label_encode import DPTRLabelEncode
 
 
 class KeepKeys(object):
