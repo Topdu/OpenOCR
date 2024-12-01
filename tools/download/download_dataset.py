3b3dcabdf101fe9756a2685a3e1f9a98cb72e6f7
ir2718 <irep2718@gmail.com>
2024-12-01 15:50:59 +0100

    update datasets

--- tools/download/download_dataset.py
+++ tools/download/download_dataset.py
@@ -1,7 +1,5 @@
 import os
 import sys
-import zipfile
-import tarfile
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 
@@ -15,25 +13,12 @@ import download.utils
 from torchvision.datasets.utils import extract_archive
 
 def main(cfg):
-    urls, filename_paths, processors, check_validity = download.utils.get_dataset_info(cfg)
-    for url, filename_path, processor in zip(urls, filename_paths, processors):
+    urls, filename_paths, processor, check_validity = download.utils.get_dataset_info(cfg)
+    for url, filename_path in zip(urls, filename_paths):
         print(f"Downloading {filename_path} from {url} . . .")
-        # download.utils.urlretrieve(url=url, filename=filename_path, check_validity=check_validity)
-
-        # if filename_path.endswith(".zip"):
-        #     with zipfile.ZipFile(filename_path, 'r') as zip_ref:
-        #         file_list = zip_ref.namelist()
-        # else:
-        #     with tarfile.open(filename_path, 'r') as tar_ref:
-        #         file_list = tar_ref.getnames()
-        # file_list = [
-        #     os.path.join(cfg["root"], name) for name in file_list 
-        #     if name.endswith(os.sep) and name.count(os.sep) == 1
-        # ]
-        file_list=["data/train2014"]
-
-        # extract_archive(from_path=filename_path, to_path=cfg["root"], remove_finished=True)
-
+        download.utils.urlretrieve(url=url, filename=filename_path, check_validity=check_validity)
+        if not filename_path.endswith(".mdb"):
+            extract_archive(from_path=filename_path, to_path=cfg["root"], remove_finished=True)
         if processor:
             if isinstance(processor, dict):
                 processor_fn_name = next(iter(processor.keys()))
@@ -42,7 +27,7 @@ def main(cfg):
                 processor_fn_name = processor
                 processor_fn_kwargs = {}
             processor_fn = getattr(download.utils, processor_fn_name)
-            processor_fn(cfg, file_list, **processor_fn_kwargs)
+            processor_fn(cfg, **processor_fn_kwargs)
 
     print("Downloads finished!")
 
