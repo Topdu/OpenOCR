a6fd740718846098428b55ea5478f5182a532800
ir2718 <irep2718@gmail.com>
2024-12-10 23:20:13 +0100

    final cleanup

--- tools/download/download_dataset.py
+++ tools/download/download_dataset.py
@@ -13,21 +13,12 @@ import download.utils
 from torchvision.datasets.utils import extract_archive
 
 def main(cfg):
-    urls, filename_paths, processor, check_validity = download.utils.get_dataset_info(cfg)
+    urls, filename_paths, check_validity = download.utils.get_dataset_info(cfg)
     for url, filename_path in zip(urls, filename_paths):
         print(f"Downloading {filename_path} from {url} . . .")
         download.utils.urlretrieve(url=url, filename=filename_path, check_validity=check_validity)
         if not filename_path.endswith(".mdb"):
             extract_archive(from_path=filename_path, to_path=cfg["root"], remove_finished=True)
-        if processor:
-            if isinstance(processor, dict):
-                processor_fn_name = next(iter(processor.keys()))
-                processor_fn_kwargs = dict(list(processor.items())[1:])
-            else:
-                processor_fn_name = processor
-                processor_fn_kwargs = {}
-            processor_fn = getattr(download.utils, processor_fn_name)
-            processor_fn(cfg, **processor_fn_kwargs)
 
     print("Downloads finished!")
 
