16dcbd4a49e7d78076f05fe4546e8514a4dfb0bc
ir2718 <irep2718@gmail.com>
2024-12-12 22:10:13 +0100

    revert

--- tools/create_lmdb_dataset.py
+++ tools/create_lmdb_dataset.py
@@ -5,17 +5,6 @@ from tqdm import tqdm
 import numpy as np
 import io
 from PIL import Image
-import sys
-
-__dir__ = os.path.dirname(os.path.abspath(__file__))
-
-sys.path.append(__dir__)
-sys.path.insert(0, os.path.abspath(os.path.join(__dir__, '..')))
-sys.path.insert(0, os.path.abspath(os.path.join(__dir__, '..', '..')))
-
-from engine import Config
-from utility import ArgsParser
-
 """ a modified version of CRNN torch repository https://github.com/bgshih/crnn/blob/master/tool/create_dataset.py """
 
 
@@ -107,19 +96,9 @@ def createDataset(data_list, outputPath, checkValid=True):
     writeCache(env, cache)
     print('Created dataset with %d samples' % nSamples)
 
-def main():
-    FLAGS = ArgsParser().parse_args()
-    cfg = Config(FLAGS.config)
-    FLAGS = vars(FLAGS)
-    opt = FLAGS.pop('opt')
-    cfg.merge_dict(FLAGS)
-    cfg.merge_dict(opt)
-    dataset_dir = os.path.join(cfg.cfg["root"], cfg.cfg["dataset_name"])
-    os.makedirs(dataset_dir, exist_ok=True)
 
 if __name__ == '__main__':
     data_dir = './Union14M-L/'
-    # downloading the filtered_label_list from https://drive.google.com/drive/folders/1x1LC8C_W-Frl3sGV9i9_i_OD-bqNdodJ?usp=drive_link
     label_file_list = [
         './Union14M-L/train_annos/filter_jsonl_mmocr0.x/filter_train_challenging.jsonl.txt',
         './Union14M-L/train_annos/filter_jsonl_mmocr0.x/filter_train_easy.jsonl.txt',
